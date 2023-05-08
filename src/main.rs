ee3236fb5ab4d329f82914c564c9ae14244aff59
Darryl Mocek <dmocek@sloocetech.net>
2023-05-07 20:40:38 -0700

    Add additional files to Cargo.toml.  Log to syslog if possible.

--- src/main.rs
+++ src/main.rs
@@ -23,12 +23,14 @@ mod parsing;
 mod windows;
 
 use anyhow::{bail, Context, Result};
+// use log::{debug, error, SetLoggerError, Level, LevelFilter, log_enabled, info};
 use std::{env, thread, time};
-use std::io::{ErrorKind, Read, Write};
+use std::io::{Read, Write};
 use std::os::unix::net::{UnixListener, UnixStream};
-use std::os::unix::process::CommandExt;
 use std::process;
 use std::process::Command;
+use env_logger::Logger;
+use syslog::{Facility, Formatter3164};
 use xcb::Connection;
 
 pub static RAISEN_SOCKET: &'static str = "/tmp/raisen.sock";
@@ -39,10 +41,10 @@ fn exec_program(prog: &str, args: &[String]) {
     let child_res = Command::new(prog).args(args).spawn();
 
     match child_res {
-        Ok(child) => {},
+        Ok(child) => {log::info!("Spawning with id: {:?}", child.id())},
         Err(error) => match error.kind() {
             other_error => {
-                println!("Problem exec'ing program: {:?}", other_error);
+                log::error!("Problem exec'ing program: {:?}", other_error);
             }
         },
     };
@@ -54,27 +56,46 @@ fn handle_stream(mut unix_stream: UnixStream) -> anyhow::Result<()> {
         .read_to_string(&mut message)
         .context("Failed at reading the unix stream")?;
 
-    println!("We received this message: {}\n", message);
+    log::info!("We received this message: {}\n", message);
     run_raise_cycle(message)?;
     Ok(())
 }
 
 fn main() -> anyhow::Result<()> {
+    let formatter = Formatter3164 {
+        facility: Facility::LOG_USER,
+        hostname: None,
+        process: "raisen".into(),
+        pid: 0,
+    };
+
+    let _logger = Logger::from_default_env();
+
+    // Get the default env_logger
+    let _res = match syslog::unix(formatter) {
+        Ok(res) => {
+            log::info!("Using syslog as the logger.", );
+            let _logger= res;
+        },
+        Err(_res) => {
+            log::error!("Cannot connect to syslog, using default logger.", );
+        },
+    };
+
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
         println!("Usage: raisen --daemon");
         println!("Usage: raisen 'class~\"Google-chrome\"' google-chrome");
-        // process::exit(1);
         return Ok(());
     } else if args.len() == 2 &&  PARAM_DAEMON.eq(&args[1]) {
-            println!("Running as daemon...");
+            log::info!("Running as daemon...");
 
             if let Err(err) = run_daemon() {
-                eprintln!("{}: {}", env!("CARGO_BIN_NAME"), err);
+                log::error!("{}: {}", env!("CARGO_BIN_NAME"), err);
                 err.chain()
                     .skip(1)
-                    .for_each(|cause| eprintln!("caused by:\n{}", cause));
+                    .for_each(|cause| log::error!("caused by:\n{}", cause));
                 let ten_millis = time::Duration::from_secs(10);
                 thread::sleep(ten_millis);
                 process::exit(1);
@@ -82,7 +103,7 @@ fn main() -> anyhow::Result<()> {
 
         return Ok(());
     } else {
-        println!("Running as client...");
+        log::info!("Running as client...");
 
         let mut unix_stream =
             UnixStream::connect(RAISEN_SOCKET).context("Could not connect to raisen socket.")?;
@@ -93,19 +114,17 @@ fn main() -> anyhow::Result<()> {
 }
 
 fn print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>())
+    log::info!("{}", std::any::type_name::<T>())
 }
 
 fn run_raise_cycle(args_str: String) -> Result<()> {
     let args: Vec<_> = env::args().collect();
     print_type_of(&args);
-    // let app = &args[0];
 
-    println!("argsStr: {}", args_str);
+    log::info!("argsStr: {}", args_str);
     let exec_args: Vec<_> = args_str.split(",").map(str::to_string).collect();
     print_type_of(&exec_args);
-    // let exec_args: Vec<_> = gs().collect();
-    println!("exec_args: {:?}", exec_args);
+    log::info!("exec_args: {:?}", exec_args);
     let app = &exec_args[0];
 
     let (condition, prog, prog_args) = if exec_args.len() >= 3 {
@@ -115,7 +134,6 @@ fn run_raise_cycle(args_str: String) -> Result<()> {
     };
 
     let cond = condition.parse()?;
-
     let (conn, screen_num) = Connection::connect(None)?;
     let screen = conn.get_setup().roots().nth(screen_num as usize).unwrap();
 
@@ -130,7 +148,7 @@ fn run_raise_cycle(args_str: String) -> Result<()> {
 fn run_daemon() -> anyhow::Result<()> {
     loop {
         if std::fs::metadata(RAISEN_SOCKET).is_ok() {
-            println!("A socket is already present. Deleting...");
+            log::warn!("A socket is already present. Deleting...");
             std::fs::remove_file(RAISEN_SOCKET).with_context(|| {
                 format!("could not delete previous socket at {:?}", RAISEN_SOCKET)
             })?;
@@ -156,10 +174,9 @@ fn write_request(unix_stream: &mut UnixStream, args: Vec<String>) -> anyhow::Res
         .collect::<Vec<_>>()
         .join(",");
 
-    println!("argsStr: {}", args_str);
+    log::info!("argsStr: {}", args_str);
 
     unix_stream
-        // .write(args.into_iter().collect()::<String>().as_bytes())
         .write(args_str.as_bytes())
         .context("Failed at writing onto the unix stream")?;
 
